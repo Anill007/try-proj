6cfd520cf2c29117e99ab1c1266c490371927165
Vivek Sitaula <bibekstl@gmail.com>
2024-11-30 17:14:27 +0545

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import IeltsReadingMain from './modules/ielts-reading/reading-main'
+import './styles/global.css'
+
+function App() {
+
+  return (
+    <div className="app-container">
+      <IeltsReadingMain></IeltsReadingMain>
+    </div>
+  )
+}
+
+export default App
---
+++ src/modules/ielts-reading/reading-main.tsx
@@ -0,0 +1,73 @@
+import './reading-main.css'
+
+function IeltsReadingMain() {
+
+    let passage = `
+    Voluptatum minima distinctio veniam reprehenderit quibusdam nam cupiditate consequuntur delectus alias? Vero laudantium laborum fuga cumque, excepturi laboriosam nisi amet dolores quibusdam, iusto dolorum qui veniam natus, asperiores iure labore.
+    Esse perferendis earum porro quas mollitia, nostrum ducimus delectus. Totam tempore accusantium doloribus, aliquid blanditiis sed, cum laudantium atque similique explicabo earum doloremque dolores deserunt amet eius hic nam sit.
+    Blanditiis itaque fugit quas aliquam reiciendis iste impedit voluptatum hic fuga provident molestias reprehenderit ea, aut qui similique corrupti dolorum rem. Mollitia quisquam ex pariatur ea quis praesentium suscipit illum?
+    Non laborum quibusdam aperiam perferendis suscipit cupiditate accusamus. Vero iusto doloremque est et mollitia a corrupti, ratione accusamus, unde recusandae perferendis fugit porro vel praesentium vitae ipsam, possimus quia exercitationem.
+    Totam mollitia incidunt nesciunt quisquam voluptatibus dolore distinctio id, fugiat impedit iure maxime natus velit explicabo fugit fuga molestias ullam. Quia et natus, nisi tempora libero odio incidunt modi totam.
+    Veniam, maiores. Laborum enim nobis blanditiis tempore nam distinctio quasi repellat? Odit et harum, soluta aliquid non ea aperiam voluptas perspiciatis? Qui officiis aliquid maiores eveniet modi tempore sequi recusandae!
+    Distinctio ad soluta illum mollitia cupiditate sed rem esse, facilis ullam, totam doloribus voluptatem odio. Animi soluta, voluptatum molestias provident architecto ut sint quisquam recusandae voluptatibus blanditiis porro ipsa doloribus?
+    Enim officiis suscipit saepe doloribus autem! Praesentium ea fuga ducimus perferendis nemo quam exercitationem! Dolorem optio corporis recusandae ipsa sunt accusantium iusto rerum eos et, nihil, id repellat iure asperiores?
+    Corporis velit cumque necessitatibus quae aperiam repellat, et quia mollitia, saepe, nisi voluptas. Enim doloremque eos dolor ullam iusto fuga aliquam maiores vel magnam, facere, voluptas recusandae. Distinctio, veritatis odio!
+    Ducimus voluptate voluptatem aliquam, error repellendus maiores fugit saepe nostrum eaque corporis ut. Nisi ducimus temporibus cumque illum? Mollitia consequuntur incidunt necessitatibus aspernatur cum, magnam quidem inventore exercitationem ipsum animi!
+    Aperiam amet officiis dolorem tempora voluptates fuga veniam atque culpa dolore commodi aspernatur explicabo mollitia alias odio accusantium, eius ab quibusdam consequatur rerum officia esse laboriosam hic. Itaque, voluptatem cupiditate.
+    Labore iure animi inventore ullam dignissimos unde quis facilis, delectus recusandae? Obcaecati eos saepe, assumenda rerum alias sit cupiditate magnam adipisci fuga aut nostrum voluptas eveniet omnis atque beatae modi?
+    Libero harum assumenda explicabo rem nesciunt. At vitae nostrum consectetur, voluptas nemo qui reiciendis dolorum minus. Ex, ducimus odit blanditiis cupiditate veritatis quam ea hic amet maxime culpa, soluta veniam!
+    Enim, quidem! Aliquam, atque? Ea vitae dolores, voluptate natus fugiat iste rem, qui error, aspernatur animi odio quia veritatis necessitatibus. Iure amet molestias obcaecati officia sunt repellendus qui commodi dolore.
+    Esse rerum id deserunt provident! Ratione, consequatur rem? Illum cum tempore, voluptatibus est totam, explicabo temporibus quos nisi fugiat cumque sunt, laborum ipsam quae? Asperiores dolore ipsam animi perspiciatis totam!
+    Facilis eaque pariatur ipsa temporibus aliquam animi ullam iste quidem itaque enim natus dolorum nobis quos, explicabo magnam dignissimos soluta quam! Saepe ab vel delectus sit, aliquid quam velit suscipit!
+    Laudantium cupiditate libero velit ea corporis minus cumque odio repudiandae consectetur minima. Ut eius officiis quisquam repellendus possimus porro accusantium illum rem, atque, accusamus odio tempora quibusdam praesentium illo qui.
+    Facere aliquid excepturi minima repudiandae praesentium, deleniti in tempora corrupti. Temporibus mollitia cumque unde reprehenderit quos consectetur, in dolores sequi soluta voluptatum iste fugiat suscipit rem, aspernatur magni asperiores aliquam.
+    Vel ratione doloremque voluptas optio nisi reiciendis aspernatur tempora fugit iste inventore adipisci perferendis reprehenderit voluptatum aliquam quasi illo nobis consequatur, dolore harum obcaecati minus ullam quo consequuntur! Enim, autem.`;
+
+    let tableData: any = [[['1'], ['2'], ['3-'], ['4']],[['5'], ['6'], ['7'], ['8']], [['9'], ['10-'], ['1-1'], ['12']], [['13'], ['14'], ['15'], ['-16']], [['17'], ['18'], ['19'], ['20']]]
+  return (
+    <div className="reading-main-container">
+        <div className="page-title">
+            <h1>Ielts Reading Test</h1>
+        </div>
+        <div className="page-body">
+            <div className="passage-container">
+                {passage}
+                {passage}
+                {passage}
+                {passage}
+            </div>
+            <div className="questions-container">
+            <table>
+                <tr>
+                    <th colSpan={4}>Research Findings</th>
+                </tr>
+                {
+                    tableData.map((row) => (
+                        <tr>
+                            {row.map((data) => (
+                            <td>
+                                {data[0].includes("-") ? (
+                                    <>
+                                        {data[0].split("-")[0]}
+                                        <input type="text" />
+                                        {data[0].split("-")[1]}
+                                    </>
+                                ) : (
+                                    data
+                                )}
+                            </td>
+                            ))}
+                        </tr>
+                    ))
+                }
+            </table>
+            </div>
+        </div>
+        <div className="page-footer">
+            <button>next</button>
+        </div>
+    </div>
+  )
+}
+
+export default IeltsReadingMain
