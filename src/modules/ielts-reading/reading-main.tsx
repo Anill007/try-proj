74439350a3d43a814d4052e12f5f3580a0b78afc
Anill007 <anil.123aaaaaa@gmail.com>
2024-12-06 15:01:20 +0545

    test

--- src/modules/ielts-reading/reading-main.tsx
+++ src/modules/ielts-reading/reading-main.tsx
@@ -2,33 +2,52 @@ import { useState } from 'react';
 import './reading-main.css'
 
 function IeltsReadingMain() {
-    let answers: string[] = ['am', 'are', 'Life', 'wow'];
+    let [currentQues, currentQuesState] = useState(0);
+    let answersArray = [['am', 'are', 'is'], ['is', 'are', 'a'], ['it'], ['is', 'it']];
+    let passageArray = [[`Voluptatum minima distinctioi 
+    reiciendis aspernatur tempora 
+    fugit iste inventore adipisci 
+    fugit iste inventore adipisci 
+    fugit iste inventore adipisci 
+    fugit iste inventore adipisci 
+    fugit iste inventore adipisci
+    nobis consequatur, dolore harum`], [`Voluptatum 
+    fugit iste inventore adipisci 
+    fugit iste inventore adipisci 
+    fugit iste inventore adipisci 
+    fugit iste inventore adipisci minima distinctioi 
+    reiciendis aspernatur tempora 
+    fugit iste inventor`], [`isci
+    nobis consequatur, dolore harum`], [`Voluptatum 
+    fugit iste inventore adipisci 
+    fugit iste inventore adipisci 
+    fugit iste inventore adipisci 
+    fugit iste inventore adipisci minima distinctioi 
+    reiciendis aspernatur tempora 
+    fugit iste inventore adipisci
+    nobis consequatur, dolore harum
+    asdasdas sadasda sadasd asdas 
+    fugit iste inventore adipisci 
+    fugit iste inventore adipisci 
+    fugit iste inventore adipisci 
+    fugit iste inventore adipisci 
+    fugit iste inventore adipiscid asdasd
+     asdasd asdasd asdasd asdasd asdasda asdasd`]];
+    let tableDataArray = [
+        [[['1'], ['2'], ['I - a boy.'], ['4']], [['5'], ['6'], ['7'], ['8']], [['9'], ['They - going to school.'], ['She - not like me.'], ['12']], [['13'], ['14'], ['15'], ['']], [['17'], ['18'], ['19'], ['20']]],
+        [[['This - a test'], ['2'], ['3333'], ['4444']], [['You - beautiful.'], ['66666'], ['7'], ['88888']], [['9999'], [''], ['Life is a race.'], ['12']], [['13'], ['14'], ['15'], ['I am not going']], [['17'], ['18'], ['19'], ['He is - wise man.']]],
+        [[['1'], ['2'], ['33'], ['Take - easy.']], [['5'], ['6'], ['7'], ['8']], [['9'], ['55555'], ['asdad'], ['12']], [['13'], ['14'], ['15'], ['sadasd']], [['17'], ['18'], ['19'], ['20']]],
+        [[['1'], ['2'], ['3333333'], ['4']], [['5'], ['6'], ['I think this - working.'], ['8']], [['keep - up folks.'], ['5'], ['543'], ['12']], [['13'], ['14'], ['15'], ['asdad']], [['17'], ['18'], ['19'], ['20']]]
+    ]
+
+
+    let answers: string[] = answersArray[currentQues];
 
     let [userAnswers, userAnswersState] = useState<string[]>(Array.from({ length: answers.length }, (_) => ''));
     let [showAnswerFlag, showAnswerFlagState] = useState(false);
 
-    let passage = `
-    Voluptatum minima distinctio veniam reprehenderit quibusdam nam cupiditate consequuntur delectus alias? Vero laudantium laborum fuga cumque, excepturi laboriosam nisi amet dolores quibusdam, iusto dolorum qui veniam natus, asperiores iure labore.
-    Esse perferendis earum porro quas mollitia, nostrum ducimus delectus. Totam tempore accusantium doloribus, aliquid blanditiis sed, cum laudantium atque similique explicabo earum doloremque dolores deserunt amet eius hic nam sit.
-    Blanditiis itaque fugit quas aliquam reiciendis iste impedit voluptatum hic fuga provident molestias reprehenderit ea, aut qui similique corrupti dolorum rem. Mollitia quisquam ex pariatur ea quis praesentium suscipit illum?
-    Non laborum quibusdam aperiam perferendis suscipit cupiditate accusamus. Vero iusto doloremque est et mollitia a corrupti, ratione accusamus, unde recusandae perferendis fugit porro vel praesentium vitae ipsam, possimus quia exercitationem.
-    Totam mollitia incidunt nesciunt quisquam voluptatibus dolore distinctio id, fugiat impedit iure maxime natus velit explicabo fugit fuga molestias ullam. Quia et natus, nisi tempora libero odio incidunt modi totam.
-    Veniam, maiores. Laborum enim nobis blanditiis tempore nam distinctio quasi repellat? Odit et harum, soluta aliquid non ea aperiam voluptas perspiciatis? Qui officiis aliquid maiores eveniet modi tempore sequi recusandae!
-    Distinctio ad soluta illum mollitia cupiditate sed rem esse, facilis ullam, totam doloribus voluptatem odio. Animi soluta, voluptatum molestias provident architecto ut sint quisquam recusandae voluptatibus blanditiis porro ipsa doloribus?
-    Enim officiis suscipit saepe doloribus autem! Praesentium ea fuga ducimus perferendis nemo quam exercitationem! Dolorem optio corporis recusandae ipsa sunt accusantium iusto rerum eos et, nihil, id repellat iure asperiores?
-    Corporis velit cumque necessitatibus quae aperiam repellat, et quia mollitia, saepe, nisi voluptas. Enim doloremque eos dolor ullam iusto fuga aliquam maiores vel magnam, facere, voluptas recusandae. Distinctio, veritatis odio!
-    Ducimus voluptate voluptatem aliquam, error repellendus maiores fugit saepe nostrum eaque corporis ut. Nisi ducimus temporibus cumque illum? Mollitia consequuntur incidunt necessitatibus aspernatur cum, magnam quidem inventore exercitationem ipsum animi!
-    Aperiam amet officiis dolorem tempora voluptates fuga veniam atque culpa dolore commodi aspernatur explicabo mollitia alias odio accusantium, eius ab quibusdam consequatur rerum officia esse laboriosam hic. Itaque, voluptatem cupiditate.
-    Labore iure animi inventore ullam dignissimos unde quis facilis, delectus recusandae? Obcaecati eos saepe, assumenda rerum alias sit cupiditate magnam adipisci fuga aut nostrum voluptas eveniet omnis atque beatae modi?
-    Libero harum assumenda explicabo rem nesciunt. At vitae nostrum consectetur, voluptas nemo qui reiciendis dolorum minus. Ex, ducimus odit blanditiis cupiditate veritatis quam ea hic amet maxime culpa, soluta veniam!
-    Enim, quidem! Aliquam, atque? Ea vitae dolores, voluptate natus fugiat iste rem, qui error, aspernatur animi odio quia veritatis necessitatibus. Iure amet molestias obcaecati officia sunt repellendus qui commodi dolore.
-    Esse rerum id deserunt provident! Ratione, consequatur rem? Illum cum tempore, voluptatibus est totam, explicabo temporibus quos nisi fugiat cumque sunt, laborum ipsam quae? Asperiores dolore ipsam animi perspiciatis totam!
-    Facilis eaque pariatur ipsa temporibus aliquam animi ullam iste quidem itaque enim natus dolorum nobis quos, explicabo magnam dignissimos soluta quam! Saepe ab vel delectus sit, aliquid quam velit suscipit!
-    Laudantium cupiditate libero velit ea corporis minus cumque odio repudiandae consectetur minima. Ut eius officiis quisquam repellendus possimus porro accusantium illum rem, atque, accusamus odio tempora quibusdam praesentium illo qui.
-    Facere aliquid excepturi minima repudiandae praesentium, deleniti in tempora corrupti. Temporibus mollitia cumque unde reprehenderit quos consectetur, in dolores sequi soluta voluptatum iste fugiat suscipit rem, aspernatur magni asperiores aliquam.
-    Vel ratione doloremque voluptas optio nisi reiciendis aspernatur tempora fugit iste inventore adipisci perferendis reprehenderit voluptatum aliquam quasi illo nobis consequatur, dolore harum obcaecati minus ullam quo consequuntur! Enim, autem.`;
-    let tableData: any = [[['1'], ['2'], ['I - a boy.'], ['4']], [['5'], ['6'], ['7'], ['8']], [['9'], ['They - going to school.'], ['- is a race.'], ['12']], [['13'], ['14'], ['15'], ['- xiii']], [['17'], ['18'], ['19'], ['20']]]
-
+    let passage = passageArray[currentQues];
+    let tableData: any = tableDataArray[currentQues];
     let inputIndexCount = 0;
 
     function updateAnswers(index: number, value: string) {
@@ -50,6 +69,17 @@ function IeltsReadingMain() {
     function resetAnswer() {
         showAnswerFlagState(false);
         userAnswersState(Array.from({ length: answers.length }, (_) => ''));
+    }
+
+    function nextQues() {
+        resetAnswer();
+        currentQuesState(prev => {
+            if (prev === 3) {
+                return 0;
+            } else {
+                return prev + 1;
+            }
+        });
     }
 
     return (
@@ -94,6 +124,10 @@ function IeltsReadingMain() {
                     <br />
                     <button onClick={resetAnswer}>reset answer</button>
                     <br />
+                    <br />
+                    {showAnswerFlag && <button onClick={nextQues}>next</button>}
+                    <br />
+                    <br />
                     {showAnswerFlag &&
                         answers.map((_, index) => {
                             return <div>{index + 1}: {_}</div>
@@ -102,7 +136,6 @@ function IeltsReadingMain() {
                 </div>
             </div>
             <div className="page-footer">
-                <button>next</button>
             </div>
         </div>
     )
